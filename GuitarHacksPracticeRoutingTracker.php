5f58d0f4c4538c09fc101c845a04e48e0af509b7
Giorgio Regni <giorgio@3i7.net>
2020-06-22 18:19:13 -0700

    Updated description

--- GuitarHacksPracticeRoutingTracker.php
+++ GuitarHacksPracticeRoutingTracker.php
@@ -7,7 +7,7 @@
 /*
 Plugin Name: Guitar Hacks Practice Routine Tracker
 Plugin URI: https://github.com/GiorgioRegni/GuitarPracticeRoutineTracker
-Description: This plugins implements site specific code for www.guitarhacks.com
+Description: This plugins implements a Guitar Practice Routine Tracker WebApp
 Version: 1.0
 Author: Giorgio Regni
 Author URI: http://github.com/GiorgioRegni/
